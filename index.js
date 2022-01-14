3d0353ddf92b0f62ca307f2edace8eeb6ffcfd2e
Felipa Mendez <felipalouise@gmail.com>
2022-01-14 10:23:17 -0600

    commit tree 59510b257c1d50b320089de4e97c418dd374923b

--- index.js
+++ index.js
@@ -10,7 +10,26 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+// need to split words in each array, not array, so gotta iterate that somehow??
+// const titleCased (tutorials) => {
+//   let tutorials[i] = tutorials[i idk].split(" ") //apparently dont need i with map, need i with original for loop syntax. map doesnt need one bc callback func, candyList.map((candy) = {}) it feeds each candy into callback function
+//   for (let i = 0; i < words.length; i += 1) {
+//   const titleCasedDeclatation //somehow declare the function  
+//   }
+//   return tutorials
+// }
+
+// const titleCasedDeclaration = tutorials.map((word) => word[0].toUpperCase)
+
+
+
+// console.log(titleCasedDeclaration)
 
 const titleCased = () => {
-  return tutorials
+return tutorials.map(tutorial =>
+  {
+    return tutorial.split(" ").map(word => word.substring(0,1).toUpperCase() + word.substring(1, word.length)).join(' ')
+  })
 }
+
+titleCased(tutorials);
